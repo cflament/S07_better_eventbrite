e5268714291bce57aba1d0b1cc2a3a7865800201
cflament <flament.cha@gmail.com>
2019-11-08 11:55:19 +0100

    edit event + access restrictions

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,8 +1,9 @@
 class EventsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_event, only: [:destroy, :edit, :show]
+  before_action :set_event, only: [:destroy, :edit, :update, :show]
+  before_action :has_access?, only: [:edit, :update, :destroy]
   before_action :set_administrator, only: [:edit]
-
+  
   def index
     @events = Event.all
   end
@@ -15,7 +16,7 @@ class EventsController < ApplicationController
     @event = Event.new(event_params)
     puts event_params
 
-    if @event.save
+    if @event.save!
       flash[:success] = "Ton événement a bien été créé"
       redirect_to event_path(@event.id)
     else
@@ -25,14 +26,26 @@ class EventsController < ApplicationController
   end
 
   def edit
+    puts params
   end
+
+  def update
+    puts event_params
+    if @event.update!(event_params)
+      flash[:success] = "Ton événement a bien été modifié"
+      redirect_to event_path(@event)
+    else
+      flash[:danger] = "L'événement n'a pas pu être modifié"
+      render edit_event_path(@event)
+    end
+  end 
 
   def show
   end
 
   def destroy
     deleted_event_title = @event.title
-    @event.destroy
+    @event.destroy!
     flash[:warning] = "Ton évènement '#{deleted_event_title}' a bien été supprimé. Tu te sens mieux ?"
     redirect_to events_path
   end
@@ -51,10 +64,4 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
-  def administrator?
-    unless 
-      flash[:danger] = "Tu ne peux pas consulter cette page profil utilisateur."
-      redirect_to root_path
-    end 
-  end
 end
