5d379732a4a0a0770606c8ef31014e75dd1ef0ea
Gaël DUBOIS <gael.dubois.pro@gmail.com>
2019-11-09 23:30:05 +0100

    updated with DESTROY method

--- app/controllers/profile_pictures_controller.rb
+++ app/controllers/profile_pictures_controller.rb
@@ -1,16 +1,24 @@
 # New controller for profile_pictures management
 class ProfilePicturesController < ApplicationController
   # callbacks : BEFORE
-  before_action :set_user, only: [:create]
+  before_action :set_user, only: [:create, :destroy]
 
   # adding a new profile picture
   def create
-    if @user.profile_picture.attach(params[:profile_picture])
+    @user.profile_picture.attach(params[:profile_picture])
+    if @user.profile_picture.attached?
       flash[:success] = "C'est tellement beeeaaauuuu..."
     else
       flash[:danger] = "Tiens, ça n'a pas marché... Essaie encore ?"
     end
     redirect_to(user_path(@user))
+  end
+
+  # removing profile picture
+  def destroy
+    @user.profile_picture.purge
+    flash[:warning] = 'Photo de profil supprimée'
+    redirect_to @user
   end
 
   private
