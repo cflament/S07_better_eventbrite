999584a35f0e053fe1dd3f48a74e1af2d9fe2f57
Gaël DUBOIS <gael.dubois.pro@gmail.com>
2019-11-09 16:40:48 +0100

    profile_pictures controller - CREATE method added - first draft

--- app/controllers/profile_pictures_controller.rb
+++ app/controllers/profile_pictures_controller.rb
@@ -1,2 +1,23 @@
+# New controller for profile_pictures management
 class ProfilePicturesController < ApplicationController
+  # callbacks : BEFORE
+  before_action :set_user, only[:create]
+
+  # adding a new profile picture
+  def create
+    if @user.avatar.attach(params[:avatar])
+      flash[:success] = "C'est tellement beeeaaauuuu..."
+    else
+      flash[:danger] = "Tiens, ça n'a pas marché... Essaie encore ?"
+    end
+    redirect_to(user_path(@user))
+  end
+
+  private
+
+  # set_user callback, because callbacks are cool
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
 end
