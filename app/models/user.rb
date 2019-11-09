f9bd49f57c03c502bab5cb554a5d3a11756b46a1
Gaël DUBOIS <gael.dubois.pro@gmail.com>
2019-11-09 16:33:10 +0100

    added comment to change in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
     has_many :attendances
     has_many :events, through: :attendances
     has_many :administrated_events, foreign_key: 'administrator_id', class_name: "Event"
-    has_one_attached :profile_picture
+    has_one_attached :profile_picture # ACTIVE_STORAGE ASSOCIATION WITH profile_picture
 
     after_create :welcome_send
 
