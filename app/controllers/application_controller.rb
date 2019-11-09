a4a9926699d12bf90e2a3ef4c582881c8f11373c
Gaël DUBOIS <gael.dubois.pro@gmail.com>
2019-11-09 21:49:36 +0100

    permitted cutom fields in DEVISE strong parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,24 @@
 class ApplicationController < ActionController::Base
-    include UsersHelper
-    include EventsHelper
-    
-    private
+  include UsersHelper
+  include EventsHelper
 
-    def has_access?
-        unless is_administrator?(@event)
-          flash[:danger] = "NON"
-          redirect_back(fallback_location: root_path)
-        end
-      end
+  # added to authorize custom fields in devise forms
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # adding the custom fields for registration
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :description])
+  end
+
+
+  private
+
+  def has_access?
+    unless is_administrator?(@event)
+      flash[:danger] = "NON"
+      redirect_back(fallback_location: root_path)
+    end
+  end
 end
