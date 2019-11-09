5689f1e01501adcb5adca0b91ba44e515821ac10
Gaël DUBOIS <gael.dubois.pro@gmail.com>
2019-11-09 16:14:47 +0100

    added has_one_attached association with active_storage profile_picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
     has_many :attendances
     has_many :events, through: :attendances
     has_many :administrated_events, foreign_key: 'administrator_id', class_name: "Event"
+    has_one_attached :profile_picture
 
     after_create :welcome_send
 
