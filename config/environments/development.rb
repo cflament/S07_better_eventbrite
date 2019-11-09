77ad40d666310e978eec1d0017789dfba7af7952
Gaël DUBOIS <gael.dubois.pro@gmail.com>
2019-11-10 00:32:00 +0100

    added line config.active_job.queue_adapter = :inline to config/environments/development to be able to seed profile pictures

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,4 +62,7 @@ Rails.application.configure do
   # config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  # workaround to be able to seed random pictures with active storage
+  config.active_job.queue_adapter = :inline
 end
